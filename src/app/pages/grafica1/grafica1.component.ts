9d358cd0f543844723a37c0c601b75165a064960
Jeisson Quintero <jeisson.quintero@doctus.com.co>
2022-06-11 14:09:36 -0500

    fin sección 6

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-grafica1',
@@ -12,5 +13,21 @@ export class Grafica1Component implements OnInit {
 
   ngOnInit(): void {
   }
+
+  public labels0: string[] = ['Venta1', 'Venta2', 'Otros'];
+  public data0: ChartData<'doughnut'> = {
+    labels: this.labels0,
+    datasets: [
+      { data: [ 20, 30, 50 ] }
+    ]
+  };
+
+  public labels1: string[] = ['Pan', 'Refresco', 'Tacos'];
+  public data1: ChartData<'doughnut'> = {
+    labels: this.labels1,
+    datasets: [
+      { data: [ 20, 15, 40 ] }
+    ]
+  };
 
 }
